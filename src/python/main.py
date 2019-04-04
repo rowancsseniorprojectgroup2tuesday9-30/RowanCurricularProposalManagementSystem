4f6720e7b62f7169ca8b066856b5fd9b47d182be
Alexander Kulpin <alexanderkulpin@rocketmail.com>
2019-04-04 01:30:25 -0400

    HTML filled in information
    All information regarding proposals filled in

--- src/python/main.py
+++ src/python/main.py
@@ -1,25 +1,25 @@
 import os
 import subprocess
 from flask import Flask, request, redirect, url_for, send_from_directory
-from flaskext.mysql import MySQL
+#from flaskext.mysql import MySQL
 from werkzeug import secure_filename
 
-UPLOAD_FOLDER = '/tmp/upload'
+UPLOAD_FOLDER = '/'
 ALLOWED_EXTENSIONS = set(['pdf', 'docx'])
 
 app = Flask(__name__)
-mysql = MySQL()
-app.config['MYSQL_DATABASE_USER'] = 'root'
-app.config['MYSQL_DATABASE_PASSWORD'] = 'senior'
-app.config['MYSQL_DATABASE_DB'] = 'curricular_management'
-app.config['MYSQL_DATABASE_HOST'] = 'localhost'
-mysql.init_app(app)
+#mysql = MySQL()
+#app.config['MYSQL_DATABASE_USER'] = 'root'
+#app.config['MYSQL_DATABASE_PASSWORD'] = 'senior'
+#app.config['MYSQL_DATABASE_DB'] = 'curricular_management'
+#app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+#mysql.init_app(app)
 
-conn = mysql.connect()
-cursor =conn.cursor()
+#conn = mysql.connect()
+#cursor =conn.cursor()
 
-cursor.execute("SELECT * from proposal")
-data = cursor.fetchone()
+#cursor.execute("SELECT * from proposal")
+#data = cursor.fetchone()
 
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
@@ -38,14 +38,10 @@ def index():
             if filename.endswith('.pdf'):
                 uname = "Kubach"
                 doctype = "-s"
-                cmd = "./archiver.sh -n " + uname + " -f " + filename + " " + doctype
-                subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE)
-                cursor.execute("INSERT INTO assessement_form_revision (assessement_form_id, assessement_form_file_path, assessement_form_datetime) values (1, 'test', NOW())")
-                res = cursor.fetchall()
-
-                for x in res:
-                    print(x)
-
+                #cmd = "./archiver.sh -n " + uname + " -f " + filename + " " + doctype
+                #subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE)
+                #cursor.execute("INSERT INTO assessement_form_revision (assessement_form_id, assessement_form_file_path, assessement_form_datetime) values (1, 'test', NOW())")
+                #res = cursor.fetchall()
 
             if filename.endswith('.docx'):
                 conv = "soffice --convert-to pdf /tmp/" + filename + " --outdir " + UPLOAD_FOLDER + " --headless"
@@ -63,14 +59,154 @@ def index():
 
             return redirect(url_for('index'))
     return """
-    <!doctype html>
-    <title>Upload Cover Sheet</title>
-    <h1>Upload Cover Sheet</h1>
-    <form action="" method=post enctype=multipart/form-data>
-      <p><input type=file name=file>
-         <input type=submit value=Upload>
-    </form>
-    <p>%s</p>
+    <!DOCTYPE html>
+<html>
+<head></head>
+
+<body>
+
+<button class="tablink" onclick="openPage('Home', this, '#57150B')" id="defaultOpen">Home</button>
+<button class="tablink" onclick="openPage('Upload', this, '#57150B')">Upload</button>
+<button class="tablink" onclick="openPage('Download', this, '#57150B')">Download</button>
+<button class="tablink" onclick="openPage('Status', this, '#57150B')">Status</button>
+
+<!––The Home page––> 
+<div id="Home" class="tabcontent">
+	<center>
+	<h1>Rowan Curricular Proposal Management System</h1>	
+	
+	<img src="Rowan_University_seal.svg.png" alt="Rowan University">
+	</center>
+</div>
+
+<!––The Download page––> 
+<div id="Download" class="tabcontent">
+	<center>
+	<h1>Download Page</h1>
+	</center>
+
+	<h3>Proposal</h3>
+
+	<select>
+		<option value="p1">Proposal 1</option>
+		<option value="p2">Proposal 2</option>
+		<option value="p3">Proposal 3</option>
+		<option value="p4">Proposal 4</option>
+	</select>
+
+	<h3>Cover Sheet</h3>
+
+	<select>
+		<option value="v1">Version 1</option>
+		<option value="v2">Version 2</option>
+	</select>
+
+	<h3>Library Form</h3>
+
+	<select>
+		<option value="v1">Version 1</option>
+		<option value="v2">Version 2</option>
+	</select>
+
+	<h3>Template</h3>
+
+	<select>
+		<option value="v1">Version 1</option>
+		<option value="v2">Version 2</option>
+	</select>
+
+	<h3>Supporting Documents</h3>
+
+	<select>
+		<option value="v1">Version 1</option>
+		<option value="v2">Version 2</option>
+	</select>
+
+	<h3>Program Guide</h3>
+
+	<select>
+		<option value="v1">Version 1</option>
+		<option value="v2">Version 2</option>
+	</select>
+
+	<h1>
+		<a href="#" class="button">Download</a>
+	</h1>
+</div>
+
+<!––The Upload page––> 
+<div id="Upload" class="tabcontent">
+	<center>
+	<h1>Upload Page</h1>
+	</center>
+
+	<h3>Proposal</h3>
+
+	<select>
+		<option value="p1">Proposal 1</option>
+		<option value="p2">Proposal 2</option>
+		<option value="p3">Proposal 3</option>
+		<option value="p4">Proposal 4</option>
+	</select>
+
+	<form>
+		<h3>Document Name</h3>
+		<input type="text" name="document_name"><br>
+	</form>
+
+	<h3>Document Type</h3>
+
+	<select>
+		<option value="cover_sheet">Cover Sheet</option>
+		<option value="library_form">Library Form</option>
+		<option value="template">Template</option>
+		<option value="supporting_document">Supporting Document</option>
+		<option value="program_guide">Program Guide</option>
+	</select>
+
+	<h3>Attach Files</h3>
+
+	<form action="" method=post enctype=multipart/form-data>
+		<p>
+		<input type=file name=file>
+		<input type=submit value=Upload>
+		</p>
+	</form>
+
+	<form>
+		<h3>Comments</h3>
+		<textarea name="comments" cols="100" rows="10"></textarea><br>
+	</form>
+</div>
+
+
+<!––The Status page––> 
+<div id="Status" class="tabcontent">
+	<center>
+	<h1>Status Page</h1>
+	</center>
+
+	<h3>Proposal</h3>
+
+	<select>
+		<option value="p1">Proposal 1</option>
+		<option value="p2">Proposal 2</option>
+		<option value="p3">Proposal 3</option>
+		<option value="p4">Proposal 4</option>
+	</select>
+
+	<h3>Cover Sheet</h3>
+
+	<h3>Library Form</h3>
+
+	<h3>Template</h3>
+
+	<h3>Supporting Documents</h3>
+
+	<h3>Program Guide</h3>
+</div>
+</body>
+
     """ % "<br>".join(os.listdir(app.config['UPLOAD_FOLDER'],))
 
 if __name__ == "__main__":
