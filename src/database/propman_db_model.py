77c71e8890e84b2dab726aaf6cf32ddc4cf68332
KyleButera <buterak0@students.rowan.edu>
2019-04-11 13:00:57 -0400

    Update propman_db_model.py

--- src/database/propman_db_model.py
+++ src/database/propman_db_model.py
@@ -7,23 +7,24 @@ Base = automap_base()
 Base.prepare(engine, reflect=True)
 
 #Mapped classes
-Consult_Letter = Base.classes.consult_letter
-Consult_Letter_Revision = Base.classes.consult_letter_revision
-Assessement_Form = Base.classes.assessment_form
-Assessement_Form_Revision = Base.classes.assessement_form_revision
+Consult_letter = Base.classes.consult_letter
+Consult_letter_revision = Base.classes.consult_letter_revision
+Assessement_form = Base.classes.assessment_form
+Assessement_form_Revision = Base.classes.assessement_form_revision
 Employee = Base.classes.employee
-Library_Form = Base.classes.library_form
-Library_Form_Revision = Base.classes.library_form_revision
+Library_form = Base.classes.library_form
+Library_form_revision = Base.classes.library_form_revision
 Proposal = Base.classes.proposal
 Sponsor = Base.classes.sponsor
-Supporting_Document = Base.classes.supporting_document
-Supporting_Document_Revision = Base.classes.supporting_document_revision
-Program_Guide = Base.classes.program_guide
-Program_Guide_Revision = Base.classes.program_guide_revision
+Supporting_document = Base.classes.supporting_document
+Supporting_document_revision = Base.classes.supporting_document_revision
+Program_guide = Base.classes.program_guide
+Program_guide_revision = Base.classes.program_guide_revision
 
 #Check mapped relationships
 '''
-classes = [Consult_Letter, Consult_Letter_Revision, Assessment_Form, Assessment_Form_Revision, Employee, Library_Form, Library_Form_Revision, Proposal, Sponsor, Supporting_Document, Supporting_Document_Revision, Program_Guide, Program_Guide_Revision]
+classes = [Consult_letter, Consult_letter_revision, Assessment_form, Assessment_form_revision, Employee, Library_form, Library_form_revision, Proposal, Sponsor, Supporting_document, Supporting_document_revision, Program_guide, Program_guide_revision]
+
 for c in classes:
     print('Relationships for %s' % str(c))
     print(inspect(c).relationships.items())
